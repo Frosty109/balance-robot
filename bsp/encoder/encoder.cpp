a407e337f30299e27db7948634119efc31ce30b7
Ben <mr.ben.frost@gmail.com>
2026-04-18 20:37:08 +0900

    New: added Encoder::init function to configure STM32 board and read encoder signals

--- bsp/encoder/encoder.cpp
+++ bsp/encoder/encoder.cpp
@@ -13,7 +13,7 @@ Encoder::Encoder(TIM_TypeDef*  tim,
                  uint16_t      pin_b,
                  uint32_t      rcc_tim,
                  uint32_t      rcc_gpio)
-    : time_(tim)
+    : tim_(tim)
     , gpio_(gpio)
     , pin_a_(pin_a)
     , pin_b_(pin_b)
@@ -21,7 +21,32 @@ Encoder::Encoder(TIM_TypeDef*  tim,
     , rcc_gpio_(rcc_gpio)
 {}
 
-Encoder::init()
-{
-    
+void Encoder::init()
+{   
+    RCC_APB1PeriphClockCmd(rcc_tim_, ENABLE);
+    RCC_APB2PeriphClockCmd(rcc_gpio_, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin = pin_a_ | pin_b_;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(gpio_, &GPIO_InitStructure);
+
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure{};
+    TIM_TimeBaseStructure.TIM_Prescaler = 0;
+    TIM_TimeBaseStructure.TIM_Period = PERIOD;
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(tim_, &TIM_TimeBaseStructure);
+
+    TIM_EncoderInterfaceConfig(tim_, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
+
+    TIM_ICInitTypeDef TIM_ICInitStructure{};
+    TIM_ICInitStructure.TIM_ICFilter = 10;
+    TIM_ICInit(tim_, &TIM_ICInitStructure);
+
+    TIM_ClearFlag(tim_, TIM_FLAG_Update);
+    TIM_ITConfig(tim_, TIM_IT_Update, ENABLE);
+    TIM_SetCounter(tim_, 0);
+    TIM_Cmd(tim_, ENABLE);
+
 }
